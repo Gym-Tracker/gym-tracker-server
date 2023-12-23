99adfe4c445b33d455c2d3579f9eaf8a49179c49
Josh Tucker <joshuatucker648@gmail.com>
2023-12-23 13:32:16 +0000

    Initial commit

---
+++ src/main/java/com/example/gymtrackerserver/GymTrackerServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gymtrackerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymTrackerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymTrackerServerApplication.class, args);
+	}
+
+}
