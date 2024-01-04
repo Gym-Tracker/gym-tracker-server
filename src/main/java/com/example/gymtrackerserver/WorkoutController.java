0ec6685b79ff4561b249b138d160ee3a529e9e95
jtucker2 <joshuatucker648@gmail.com>
2024-01-04 15:49:31 +0000

    Added cross origin annotation for development

--- src/main/java/com/example/gymtrackerserver/WorkoutController.java
+++ src/main/java/com/example/gymtrackerserver/WorkoutController.java
@@ -1,11 +1,14 @@
 package com.example.gymtrackerserver;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.*;
 
+// TODO: remove cross origin annotation
+@CrossOrigin(origins = "http://127.0.0.1:5173")
 @RestController
 public class WorkoutController {
 
