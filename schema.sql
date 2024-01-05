108d395ad1992d7e0b52b24978b87608ffb7e3a9
jtucker2 <joshuatucker648@gmail.com>
2024-01-05 13:29:11 +0000

    Added schema file

---
+++ schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE "user"(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    password TEXT,
+    age INT
+);
+
+CREATE TABLE workout(
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES "user"(id),
+    duration INT,
+    timestamp TIMESTAMP
+);
+
+CREATE TABLE exercise(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE set(
+    id SERIAL PRIMARY KEY,
+    workout_id INT REFERENCES workout(id),
+    exercise_id INT REFERENCES exercise(id),
+    type INT,
+    weight NUMERIC(5, 2),
+    reps INT
+);
