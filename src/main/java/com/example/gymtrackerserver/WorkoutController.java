dd2caef79f075f36983b2bc02262c1d80e9c22d3
jtucker2 <joshuatucker648@gmail.com>
2024-01-06 12:45:33 +0000

    Added creating workout objects from database

--- src/main/java/com/example/gymtrackerserver/WorkoutController.java
+++ src/main/java/com/example/gymtrackerserver/WorkoutController.java
@@ -1,11 +1,10 @@
 package com.example.gymtrackerserver;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 // TODO: remove cross origin annotation
 @CrossOrigin(origins = "http://127.0.0.1:5173")
@@ -16,6 +15,8 @@ public class WorkoutController {
     private final String url = "jdbc:postgresql://gym-tracker.ctvjp0tbn6qi.eu-west-2.rds.amazonaws.com:4323/";
     private final String user = "postgres";
     private final String password = "#V%C9Cc&P7jh59zB";
+
+    final int userID = 15;
 
     @PostMapping("/workout")
     Workout newWorkout(@RequestBody Workout newWorkout) {
@@ -28,7 +29,7 @@ public class WorkoutController {
             conn = DriverManager.getConnection(url, user, password);
 
             PreparedStatement pstmt = conn.prepareStatement(workoutSQL, Statement.RETURN_GENERATED_KEYS);
-            pstmt.setInt(1, 15);
+            pstmt.setInt(1, userID);
             pstmt.setInt(2, newWorkout.duration());
             pstmt.setTimestamp(3, newWorkout.date());
 
@@ -42,7 +43,7 @@ public class WorkoutController {
                     System.out.println(set);
                     pstmt = conn.prepareStatement(setSQL);
                     pstmt.setInt(1, workoutID);
-                    pstmt.setInt(2, 1);
+                    pstmt.setInt(2, exercise.id());
                     pstmt.setInt(3, set.type());
                     pstmt.setFloat(4, set.weight());
                     pstmt.setInt(5, set.reps());
@@ -55,6 +56,36 @@ public class WorkoutController {
         }
 
         return newWorkout;
+    }
+
+    @GetMapping("/workout")
+    Workout[] workouts() {
+        List<Workout> workouts = new ArrayList<>();
+        String SQL = "SELECT * FROM workout WHERE user_id = ?";
+
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
+            pstmt.setInt(1, userID);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getInt("duration"));
+                System.out.println(rs.getTimestamp("timestamp"));
+                workouts.add(
+                        new Workout(
+                                rs.getTimestamp("timestamp"),
+                                rs.getInt("duration"),
+                                new Exercise[] {}
+                        )
+                );
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return workouts.toArray(new Workout[0]);
     }
 
     @PostMapping("user")
